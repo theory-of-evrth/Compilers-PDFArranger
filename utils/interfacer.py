434763bcaab2e9fc0a76b167dd7321d5b598d6ed
mael brandt <mael.brandt@he-arc.ch>
2025-01-09 09:07:39 +0100

    draw

--- utils/interfacer.py
+++ utils/interfacer.py
@@ -1,12 +1,12 @@
 from reportlab.pdfgen import canvas
-
 from reportlab.lib.pagesizes import A4
+import os.path
 
 # test library present and working
 def hello(c):
-    c.setFillColor("#FF0000") 
-    c.drawString(100,100,"Hello World")
-c = canvas.Canvas("generated/hello.pdf", pagesize=A4)
+    c.setFillColor("#0000FF") 
+    c.drawString(100,100,"Hello World")    
+c = canvas.Canvas("compil/Compilers-PDFArranger/generated/hello.pdf", pagesize=A4)
 hello(c)
 c.showPage()
 c.save()
@@ -15,7 +15,7 @@ c.save()
 TODO : create functions for interfacing with the library
 needed functions:
 - create canvas
-- draw line (position, color, size)
+- draw line (position, color, size) 
 - draw circle (position, color, size)
 - draw triangle (position, color, size)
 - draw text (position, color, size, content) opt.: font (set default value)
@@ -25,11 +25,21 @@ needed functions:
 !! spumoni for overlap. Test with overlapping of different elements
 """
 
-def createCanvas():
-    pass
+def createCanvas(filename, pagesize: tuple[float, float]):
+    c = canvas.Canvas(filename, pagesize=pagesize)
+    return c
 
-def drawLine(position, color, size):
-    pass
+# TODO drawing line based on 2 point would vbe better, no ?
+def drawLine(c: canvas.Canvas, position: tuple[float, float], color: str, size: float):
+    """draw a line based on a point and a length.
+    
+    param:  
+        position : a tuple containing the coordinate of the starting point of the line
+        color    : string representing the hexadecimal value of a color (format: #hex_value)
+        size     : length of the line
+    """
+    c.setStrokeColor(aColor=color)
+    c.line(x1=position[0], y1=position[1], x2=position[0] + size, y2=position[1] + size)
 
 def drawCircle(position, color, size):
     pass
@@ -37,8 +47,10 @@ def drawCircle(position, color, size):
 def drawTriangle(position, color, size):
     pass
 
-def drawText(position, color, size, content, font=20):
-    pass
+def drawText(c: canvas.Canvas, position: tuple[float, float], color: str, content, font="Helvetica", size=20):
+    c.setFillColor(aColor=color)
+    c.setFont(psfontname=font, size=size)
+    c.drawString(x=position[0], y=position[1], text=content)
 
 def setBackgroundColor(color):
     pass
